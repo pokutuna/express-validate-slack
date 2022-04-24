85d545b16ef5db33c448306d042ad5c6b7fe49e0
pokutuna <mail@pokutuna.com>
2022-04-24 16:30:04 +0900

    fix: rm compiled files

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-import { Request, Response, RequestHandler } from "express";
-export declare function rawBodyKeeper(req: Request, res: Response, buf: Buffer): void;
-export default function verifySlack(signingSecret: string): RequestHandler;
